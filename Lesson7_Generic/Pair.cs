24f39a1f394b14356faab964cc163a1b60970533
Viktoryia Supronenko <vika.supronenko@icloud.com>
2022-11-01 16:36:58 +0300

    Add project files.

---
+++ Lesson7_Generic/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson7_Generic
+{
+    internal class Pair<T, S>
+    {
+        public T ID { get; set; }
+        public S Name { get; set; }
+
+        public Pair(T id, S name)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
---
+++ Lesson7_Generic/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Lesson7_Generic;
+
+Pair<int, string> pair = new Pair<int, string>(1, "Vika");
+Console.Write(pair.ID + "  ");
+Console.WriteLine(pair.Name);
